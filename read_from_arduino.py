7dc73220c2d59d796947051bdbeaeab439a9d3d1
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2017-01-15 18:42:51 -0300

    added comments

--- read_from_arduino.py
+++ read_from_arduino.py
@@ -2,7 +2,9 @@
 
 import serial
 
+# init serial, open arduino to check the port that the arduino is using
 ser = serial.Serial('/dev/ttyACM1', 9600, 8, 'N', 1, timeout=5)
 
+# loop the script
 while True:
     print(ser.readline())
