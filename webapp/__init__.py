bce5b9ab40757ebda0b8d94a27c673e85f879e88
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2017-01-15 22:12:43 -0300

    worked on the web

---
+++ webapp/__init__.py
@@ -0,0 +1,31 @@
+# using Python Flask Framework
+# arch users : sudo pip install flask
+
+from flask import Flask, render_template
+
+# for reading from the usb serial
+import serial
+
+app = Flask(__name__)
+
+# route to read from arduino
+@app.route("/read_from_arduino")
+def read_from_arduino():
+	# init serial, open arduino to check the port that the arduino is using
+	ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=5)
+
+	# output what the serial is saying
+	output = ser.readline()
+
+	# return the values
+	return output
+
+# render the dashboard
+@app.route("/")
+def hello():
+	# render
+	return render_template('home.html')
+
+# run the app
+if __name__ == "__main__":
+    app.run()
